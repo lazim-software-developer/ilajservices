84a6ef0ab56ee095345220dae9272e63b89ef8d9
Lazim Software Developer <140724566+lazim-software-developer@users.noreply.github.com>
2025-10-06 16:30:27 +0400

    update for google anylitics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import TagManager from 'react-gtm-module';
 
-// Initialize GTM with your GTM ID
-const tagManagerArgs = {
-  gtmId: 'G-BCY6K47PLY', // Replace this with your actual GTM ID
-};
-TagManager.initialize(tagManagerArgs);
 createRoot(document.getElementById("root")!).render(<App />);
